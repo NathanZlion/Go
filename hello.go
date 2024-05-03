855b82fe49ef7a625d7292ea85338bc2df42d3a5
Nathnael Dereje <nathandere1357@gmail.com>
2024-05-03 22:27:28 +0300

    Complete video 6 of netninga Golang beginner playlist

--- hello.go
+++ hello.go
@@ -3,6 +3,30 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+	// Arrays in Go and also other languages as you know it have fixed size.
+	// Explicit initialization
+	var age [3]int  = [3]int {12, 3, 3};
+	var also_age = [3]int {12, 3, 3}; // simpler way
+	fmt.Println(age);
+	fmt.Println(also_age);
+
+	// len : Length of an array
+	fmt.Println(age, "Length =>" , len(age));
+
+	// As you have seen arrays are of fixed size. Once initialized you cannot change the size.
+	// In python you know list right. Thq equivalent of list in Go is slice.
+	// Slices are dynamic arrays. They can grow and shrink. Under the hood slices are implemented as arrays.
+	var scores = []int {1, 2, 3};
+	fmt.Println(scores);
+
+	// Append to a slice
+	scores = append(scores, 4);
+	fmt.Println(scores, "Length =>" , len(scores));
+	fmt.Printf("The type of scores is %T\n", scores);
+
+	// Slice range
+	rangeOne := scores[0:2];
+	fmt.Println(rangeOne, "Length =>" , len(rangeOne));
+	fmt.Printf("The type of rangeOne is %T\n", rangeOne);
 }
 
