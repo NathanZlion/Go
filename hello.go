c22121cdeb164aeabafaa45aff488665175a1a66
Nathnael Dereje <nathandere1357@gmail.com>
2024-05-05 10:59:29 +0300

    Complete video 10 of netninga golang serise

--- hello.go
+++ hello.go
@@ -2,7 +2,21 @@ package main
 
 import "fmt"
 
+func sayHi(name string) {
+	fmt.Printf("Hi there %v.\n", name)
+}
+
+
+// Passing functions as function argument
+func cycleNames (names []string, function func(string)) {
+	for _, name	:= range names {
+		function(name);
+	}
+}
+
 func main() {
 	fmt.Println("Hello World")
+	names := []string {"Nathnael", "Alebachew", "Yonas"}
+	cycleNames(names, sayHi);
 }
 
