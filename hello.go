c51dcf358528addeaf003d392f382ef1b318db8a
Nathnael Dereje <nathandere1357@gmail.com>
2024-05-05 15:50:41 +0300

    Complete video 12 of the netninja golang series

--- hello.go
+++ hello.go
@@ -4,5 +4,18 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello World")
+	map_ := map[string]float64 {
+		"nathnael": 2.3,
+		"dawit": 1.3,
+		"luigi": 21.4,
+	}
+
+	fmt.Println(map_);
+	fmt.Println(map_["nathnael"]);
+
+	// traversing the map
+	for key, value := range map_ {
+		fmt.Printf("key : %s, Value : %f\n", key, value);
+	}
 }
 
